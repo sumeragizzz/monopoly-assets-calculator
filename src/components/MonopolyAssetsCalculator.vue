75e75b87fe30b228d6e96277892951f7bca197cd
sumeragizzz <developer@sumeragizzz.dev>
2023-09-13 00:33:19 +0900

    UIを全面的に見直し

--- src/components/MonopolyAssetsCalculator.vue
+++ src/components/MonopolyAssetsCalculator.vue
@@ -1,67 +1,39 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Property, Street, Railroad, Utility, Player } from '../model.ts'
+import { Game, Player, Property } from '../model.ts'
 
 defineProps<{ msg: string }>()
 
-const properties: Array<Property> = [
-  Street.HACHIOJI, Street.TACHIKAWA,
-  Street.YOTSUYA, Street.YOYOGI, Street.ICHIGAYA,
-  Street.AKIHABARA, Street.UENO, Street.IKEBUKURO,
-  Street.ODAIBA, Street.HIBIYA, Street.SHIMBASHI,
-  Street.EBISU, Street.HARAJUKU, Street.OMOTESANDO,
-  Street.AKASAKA, Street.ROPPONGI, Street.TORANOMON,
-  Street.YURAKUCHO, Street.NIHONBASHI, Street.OTEMACHI,
-  Street.MARUNOUCHI, Street.GINZA,
-  Railroad.SHINJUKU, Railroad.SHINAGAWA, Railroad.SHIBUYA, Railroad.TOKYO,
-  Utility.POWER_COMPANY, Utility.WATER_COMPANY
-]
+const game = ref(new Game())
 
-const players = ref(new Array())
 const addPlayer = () => {
-  players.value.push(new Player())
+  game.value.addPlayer()
 }
 
-const removePlayer = (index: number) => {
-  players.value.splice(index, 1)
+const removePlayer = (id: string) => {
+  // TODO 確認ダイアログを表示する
+
+  game.value.removePlayer(id)
 }
 
-// 抵当
-const mortgageProperty = (player: Player, property: Property) => {
-  // TODO 未購入状態から抵当にすることができないので要修正
-  if (!player.hasProperty(property)) {
-    player.buyProperty(property)
-  }
-  if (player.isMortgaged(property)) {
-    player.settlementProperty(property)
-  } else {
-    player.mortgageProperty(property)
-  }
+const buyProperty = (property: Property, houseCount: number) => {
+  const player: Player = game.value.getOwnedPlayer(property) ?? game.value.getAllPlayers()[0]
+  player.buyProperty(property, houseCount)
 }
 
-// 物件購入/売却
-const buyProperty = (player: Player, property: Property, houseCount: number) => {
-  if (!player.hasProperty(property)) {
-    player.buyProperty(property)
-    player.buildHouses(property, houseCount)
-  } else if (houseCount > player.getHouseCount(property)) {
-    player.buildHouses(property, houseCount - player.getHouseCount(property))
-  } else if (houseCount < player.getHouseCount(property)) {
-    player.sellHouses(property, player.getHouseCount(property) - houseCount)
-  } else if (player.isMortgaged(property)) {
-    player.settlementProperty(property)
-  } else {
-    player.sellProperty(property)
-  }
+const sellProperty = (property: Property) => {
+  const player: Player | undefined = game.value.getOwnedPlayer(property)
+  player?.sellProperty(property)
 }
 
-addPlayer()
+const mortgageProperty = (property: Property) => {
+  const player: Player = game.value.getOwnedPlayer(property) ?? game.value.getAllPlayers()[0]
+  player.mortgageProperty(property)
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <button type="button" @click="addPlayer">追加</button>
 
   <table>
     <thead>
@@ -70,57 +42,74 @@ addPlayer()
         <th>物件</th>
         <th>価格</th>
         <th>建設備</th>
-        <th v-for="(player, index) in players">
+        <th></th>
+        <th v-for="(player, playerIndex) in game.getAllPlayers()">
+          {{ playerIndex + 1 }}:&nbsp;
           <input v-model="player.name" />
-          <button @click="removePlayer(index)">削除</button>
+          <button @click="removePlayer(player.id)" :disabled="game.getAllPlayers().length <= 1">削除</button>
         </th>
+        <th><button type="button" @click="addPlayer" :disabled="game.getAllPlayers().length >= 8">追加</button></th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="property in properties">
-        <td :style="{'background-color': property.group.color}">&nbsp;</td>
-        <td>{{ property.name }}</td>
-        <td>{{ property.price }}</td>
-        <td>{{ !property.canBuildHouse() ? '-' : property.cost }}</td>
-        <template v-for="player in players">
-          <td>
-            <button @click="mortgageProperty(player, property)">抵当</button>
-            <button @click="buyProperty(player, property, 0)">0</button>
-            <button @click="buyProperty(player, property, 1)" :disabled="!property.canBuildHouse()">1</button>
-            <button @click="buyProperty(player, property, 2)" :disabled="!property.canBuildHouse()">2</button>
-            <button @click="buyProperty(player, property, 3)" :disabled="!property.canBuildHouse()">3</button>
-            <button @click="buyProperty(player, property, 4)" :disabled="!property.canBuildHouse()">4</button>
-            <button @click="buyProperty(player, property, 5)" :disabled="!property.canBuildHouse()">ホテル</button>
-            {{ player.hasProperty(property) ? player.calculatePresentValue(property) : 0 }}
-          </td>
-        </template>
-      </tr>
-    </tbody>
-    <tfoot>
       <tr>
         <td></td>
-        <td>資産小計</td>
+        <td>合計</td>
         <td></td>
         <td></td>
-        <td v-for="player in players">{{ player.calculateAllPresentValue() }}</td>
+        <td></td>
+        <td v-for="player in game.getAllPlayers()">{{ player.money + player.calculateTotalValuation() }}</td>
+        <td></td>
       </tr>
       <tr>
         <td></td>
         <td>所持金</td>
         <td></td>
         <td></td>
-        <td v-for="player in players">
-          <input v-model.number="player.money" />
-        </td>
+        <td></td>
+        <td v-for="player in game.getAllPlayers()"><input type="number" v-model.number="player.money" /></td>
+        <td></td>
       </tr>
       <tr>
         <td></td>
-        <td>合計</td>
+        <td>資産小計</td>
         <td></td>
         <td></td>
-        <td v-for="player in players">{{ player.getTotalAssets() }}</td>
+        <td></td>
+        <td v-for="player in game.getAllPlayers()">{{ player.calculateTotalValuation() }}</td>
+        <td></td>
       </tr>
-    </tfoot>
+      <tr v-for="property in game.getAllProperties()">
+        <td :style="{'background-color': property.group.color}">&nbsp;</td>
+        <td>{{ property.name }}</td>
+        <td>{{ property.price }}</td>
+        <td>{{ !property.buildable ? '-' : property.cost }}</td>
+        <td>
+          <button @click="sellProperty(property)">✕</button>
+          <button @click="buyProperty(property, 0)">0</button>
+          <button @click="buyProperty(property, 1)" :disabled="!property.buildable">1</button>
+          <button @click="buyProperty(property, 2)" :disabled="!property.buildable">2</button>
+          <button @click="buyProperty(property, 3)" :disabled="!property.buildable">3</button>
+          <button @click="buyProperty(property, 4)" :disabled="!property.buildable">4</button>
+          <button @click="buyProperty(property, 5)" :disabled="!property.buildable">ホテル</button>
+          <button @click="mortgageProperty(property)">抵当</button>
+          <select v-model="game.getPropertyStatus(property).owner">
+            <option disabled>所有者を選択してください</option>
+            <option :value="undefined"></option>
+            <option v-for="(player, playerIndex) in game.getAllPlayers()" :value="player">{{ `${playerIndex + 1}: ${player.name}` }}</option>
+          </select>
+        </td>
+        <!--
+        <template v-for="player in players">
+        </template>
+        -->
+        <!-- @vue-ignore -->
+        <td v-for="player in game.getAllPlayers()">
+          <template v-if="player.isOwnedProperty(property)">{{ game.getPropertyStatus(property).calculateValuation() }}</template>
+        </td>
+        <td></td>
+      </tr>
+    </tbody>
   </table>
 </template>
 
--- src/model.ts
+++ src/model.ts
@@ -12,6 +12,7 @@ export class Group {
 
     // 鉄道
     static readonly RAILROAD = new Group('鉄道', 4, 'black')
+
     // 公共会社
     static readonly UTILITY = new Group('公共会社', 2, 'grey')
 
@@ -27,229 +28,224 @@ export class Group {
 }
 
 // 物件
-export interface Property {
+export class Property {
+    // 土地
+    static readonly HACHIOJI: Property = new Property('八王子', 60, 50, Group.BROWN, true)
+    static readonly TACHIKAWA: Property = new Property('立川', 60, 50, Group.BROWN, true)
+    static readonly YOTSUYA: Property = new Property('四ツ谷', 100, 50, Group.LIGHTBLUE, true)
+    static readonly YOYOGI: Property = new Property('代々木', 100, 50, Group.LIGHTBLUE, true)
+    static readonly ICHIGAYA: Property = new Property('市ヶ谷', 120, 50, Group.LIGHTBLUE, true)
+    static readonly AKIHABARA: Property = new Property('秋葉原', 140, 100, Group.LIGHTPURPLE, true)
+    static readonly UENO: Property = new Property('上野', 140, 100, Group.LIGHTPURPLE, true)
+    static readonly IKEBUKURO: Property = new Property('池袋', 160, 100, Group.LIGHTPURPLE, true)
+    static readonly ODAIBA: Property = new Property('お台場', 180, 100, Group.ORANGE, true)
+    static readonly HIBIYA: Property = new Property('日比谷', 180, 100, Group.ORANGE, true)
+    static readonly SHIMBASHI: Property = new Property('新橋', 200, 100, Group.ORANGE, true)
+    static readonly EBISU: Property = new Property('恵比寿', 220, 150, Group.RED, true)
+    static readonly HARAJUKU: Property = new Property('原宿', 220, 150, Group.RED, true)
+    static readonly OMOTESANDO: Property = new Property('表参道', 240, 150, Group.RED, true)
+    static readonly AKASAKA: Property = new Property('赤坂', 260, 150, Group.YELLOW, true)
+    static readonly ROPPONGI: Property = new Property('六本木', 260, 150, Group.YELLOW, true)
+    static readonly TORANOMON: Property = new Property('虎ノ門', 280, 150, Group.YELLOW, true)
+    static readonly YURAKUCHO: Property = new Property('有楽町', 300, 200, Group.GREEN, true)
+    static readonly NIHONBASHI: Property = new Property('日本橋', 300, 200, Group.GREEN, true)
+    static readonly OTEMACHI: Property = new Property('大手町', 320, 200, Group.GREEN, true)
+    static readonly MARUNOUCHI: Property = new Property('丸ノ内', 350, 200, Group.DARKBLUE, true)
+    static readonly GINZA: Property = new Property('銀座', 400, 200, Group.DARKBLUE, true)
+
+    // 鉄道
+    static readonly SHINJUKU: Property = new Property('新宿駅', 200, 0, Group.RAILROAD, false)
+    static readonly SHINAGAWA: Property = new Property('品川駅', 200, 0, Group.RAILROAD, false)
+    static readonly SHIBUYA: Property = new Property('渋谷駅', 200, 0, Group.RAILROAD, false)
+    static readonly TOKYO: Property = new Property('東京駅', 200, 0, Group.RAILROAD, false)
+
+    // 公共会社
+    static readonly POWER_COMPANY: Property = new Property('電力会社', 150, 0, Group.UTILITY, false)
+    static readonly WATER_COMPANY: Property = new Property('水道会社', 150, 0, Group.UTILITY, false)
+
     readonly name: string
     readonly price: number
     readonly cost: number
     readonly group: Group
+    readonly buildable: boolean
 
-    canBuildHouse(): boolean
-}
-
-// 土地
-export class Street implements Property {
-    static readonly HACHIOJI: Street = new Street('八王子', 60, 50, Group.BROWN)
-    static readonly TACHIKAWA: Street = new Street('立川', 60, 50, Group.BROWN)
-    static readonly YOTSUYA: Street = new Street('四ツ谷', 100, 50, Group.LIGHTBLUE)
-    static readonly YOYOGI: Street = new Street('代々木', 100, 50, Group.LIGHTBLUE)
-    static readonly ICHIGAYA: Street = new Street('市ヶ谷', 120, 50, Group.LIGHTBLUE)
-    static readonly AKIHABARA: Street = new Street('秋葉原', 140, 100, Group.LIGHTPURPLE)
-    static readonly UENO: Street = new Street('上野', 140, 100, Group.LIGHTPURPLE)
-    static readonly IKEBUKURO: Street = new Street('池袋', 160, 100, Group.LIGHTPURPLE)
-    static readonly ODAIBA: Street = new Street('お台場', 180, 100, Group.ORANGE)
-    static readonly HIBIYA: Street = new Street('日比谷', 180, 100, Group.ORANGE)
-    static readonly SHIMBASHI: Street = new Street('新橋', 200, 100, Group.ORANGE)
-    static readonly EBISU: Street = new Street('恵比寿', 220, 150, Group.RED)
-    static readonly HARAJUKU: Street = new Street('原宿', 220, 150, Group.RED)
-    static readonly OMOTESANDO: Street = new Street('表参道', 240, 150, Group.RED)
-    static readonly AKASAKA: Street = new Street('赤坂', 260, 150, Group.YELLOW)
-    static readonly ROPPONGI: Street = new Street('六本木', 260, 150, Group.YELLOW)
-    static readonly TORANOMON: Street = new Street('虎ノ門', 280, 150, Group.YELLOW)
-    static readonly YURAKUCHO: Street = new Street('有楽町', 300, 200, Group.GREEN)
-    static readonly NIHONBASHI: Street = new Street('日本橋', 300, 200, Group.GREEN)
-    static readonly OTEMACHI: Street = new Street('大手町', 320, 200, Group.GREEN)
-    static readonly MARUNOUCHI: Street = new Street('丸ノ内', 350, 200, Group.DARKBLUE)
-    static readonly GINZA: Street = new Street('銀座', 400, 200, Group.DARKBLUE)
-
-    readonly name: string
-    readonly price: number
-    readonly cost: number
-    readonly group: Group
-
-    private constructor(name: string, price: number, cost: number, group: Group) {
+    private constructor(name: string, price: number, cost: number, group: Group, buildable: boolean) {
         this.name = name
         this.price = price
         this.cost = cost
         this.group = group
-    }
-
-    canBuildHouse(): boolean {
-        return true
+        this.buildable = buildable
     }
 }
 
-// 鉄道
-export class Railroad implements Property {
-    static readonly SHINJUKU: Railroad = new Railroad('新宿駅', 200, Group.RAILROAD)
-    static readonly SHINAGAWA: Railroad = new Railroad('品川駅', 200, Group.RAILROAD)
-    static readonly SHIBUYA: Railroad = new Railroad('渋谷駅', 200, Group.RAILROAD)
-    static readonly TOKYO: Railroad = new Railroad('東京駅', 200, Group.RAILROAD)
-
-    readonly name: string
-    readonly price: number
-    readonly cost: number
-    readonly group: Group
-
-    private constructor(name: string, price: number, group: Group) {
-        this.name = name
-        this.price = price
-        this.cost = 0
-        this.group = group
-    }
-
-    canBuildHouse(): boolean {
-        return false
-    }
-}
-
-// 公共会社
-export class Utility implements Property {
-    static readonly POWER_COMPANY: Utility = new Utility('電力会社', 150, Group.UTILITY)
-    static readonly WATER_COMPANY: Utility = new Utility('水道会社', 150, Group.UTILITY)
-
-    readonly name: string
-    readonly price: number
-    readonly cost: number
-    readonly group: Group
-
-    private constructor(name: string, price: number, group: Group) {
-        this.name = name
-        this.price = price
-        this.cost = 0
-        this.group = group
-    }
-
-    canBuildHouse(): boolean {
-        return false
-    }
-}
-
+// 物件状態
 class PropertyStatus {
     readonly property: Property
+    owner?: Player
     houseCount: number
     mortgaged: boolean
 
     constructor(property: Property) {
         this.property = property
+        this.owner = undefined
         this.houseCount = 0
         this.mortgaged = false
     }
 
-    // 評価額を算出する
-    calculatePresentValue(): number {
-        return this.property.price / (this.mortgaged ? 2 : 1) + this.property.cost * this.houseCount
+    isOwned(player?: Player): boolean {
+        if (player === undefined) {
+            return this.owner !== undefined
+        }
+        return this.owner === player
+    }
+
+    dennyProperty() {
+        this.owner = undefined
+    }
+
+    calculateValuation(): number {
+        if (!this.isOwned()) {
+            return 0
+        }
+        if (this.mortgaged) {
+            return this.property.price / 2
+        }
+        return this.property.price + this.property.cost * this.houseCount
+    }
+}
+
+export class Game {
+    private board: Board
+    private  players: Array<Player>
+
+    constructor() {
+        this.board = new Board()
+        this.players = new Array(new Player(this.board))
+    }
+
+	getAllProperties(): Array<Property> {
+        return Array.from(Board.properties)
+    }
+
+    getPropertyStatus(property: Property): PropertyStatus {
+        return this.board.getPropertyStatus(property)
+    }
+
+	addPlayer(): Player | undefined {
+        if (this.players.length >= 8) {
+            return undefined
+        }
+        const newPlayer: Player = new Player(this.board);
+        this.players.push(newPlayer)
+        return newPlayer
+    }
+
+    removePlayer(id: string): Player | undefined {
+        if (this.players.length <= 1) {
+            return undefined
+        }
+
+        const playerIndex: number = this.players.findIndex(player => player.id === id)
+        const player: Player = this.players[playerIndex]
+
+        Board.properties
+            .map(property => this.board.getPropertyStatus(property))
+            .filter(status => status.isOwned(player))
+            .forEach(status => status.dennyProperty())
+
+        this.players.splice(playerIndex, 1)
+        return player
+    }
+
+    getPlayer(id: string): Player | undefined {
+        return this.players.find(player => player.id === id)
+    }
+
+    getOwnedPlayer(property: Property): Player | undefined {
+        return this.board.getPropertyStatus(property).owner
+    }
+
+    getAllPlayers(): Array<Player> {
+        return Array.from(this.players)
+    }
+}
+
+class Board {
+    static readonly properties: Array<Property> = [
+        Property.HACHIOJI, Property.TACHIKAWA,
+        Property.YOTSUYA, Property.YOYOGI, Property.ICHIGAYA,
+        Property.AKIHABARA, Property.UENO, Property.IKEBUKURO,
+        Property.ODAIBA, Property.HIBIYA, Property.SHIMBASHI,
+        Property.EBISU, Property.HARAJUKU, Property.OMOTESANDO,
+        Property.AKASAKA, Property.ROPPONGI, Property.TORANOMON,
+        Property.YURAKUCHO, Property.NIHONBASHI, Property.OTEMACHI,
+        Property.MARUNOUCHI, Property.GINZA,
+        Property.SHINJUKU, Property.SHINAGAWA, Property.SHIBUYA, Property.TOKYO,
+        Property.POWER_COMPANY, Property.WATER_COMPANY
+    ]
+
+    private statuses: Map<Property, PropertyStatus>
+
+    constructor() {
+        this.statuses = new Map(Board.properties.map(property => [property, new PropertyStatus(property)]))
+    }
+
+    getPropertyStatus(property: Property): PropertyStatus {
+        return this.statuses.get(property)!
     }
 }
 
 // プレイヤー
 export class Player {
+    private board: Board
+    readonly id: string
     name: string
     money: number
-    private propertyStatuses: Map<string, PropertyStatus>
 
-    constructor()
+    constructor(board: Board)
 
-    constructor(name?: string) {
-        // TODO デフォルトプレイヤー名の設定
+    constructor(board: Board, name?: string) {        
+        this.board = board
+        this.id = crypto.randomUUID()
         this.name = name ?? 'プレイヤー'
         this.money = 0
-        this.propertyStatuses = new Map()
     }
 
-    // 部件を保持しているか
-    hasProperty(property: Property): boolean {
-        return this.propertyStatuses.has(property.name)
+    buyProperty(property: Property, houseCount: number): void {
+        const status: PropertyStatus = this.board.getPropertyStatus(property)
+        status.owner = this
+        status.houseCount = houseCount
+        status.mortgaged = false
     }
 
-    // 物件購入
-    buyProperty(property: Property): void {
-        if (this.propertyStatuses.has(property.name)) {
-            throw new Error()
-        }
-        this.propertyStatuses.set(property.name, new PropertyStatus(property))
-    }
-
-    // 物件売却
     sellProperty(property: Property): void {
-        if (!this.propertyStatuses.has(property.name)) {
-            throw new Error()
-        }
-        this.propertyStatuses.delete(property.name)
+        const status: PropertyStatus = this.board.getPropertyStatus(property)
+        status.owner = undefined
+        status.houseCount = 0
+        status.mortgaged = false
     }
 
-    // 抵当設定
     mortgageProperty(property: Property): void {
-        if (!this.propertyStatuses.has(property.name)) {
-            throw new Error()
-        }
-        this.propertyStatuses.get(property.name)!.mortgaged = true
+        const status: PropertyStatus = this.board.getPropertyStatus(property)
+        status.owner = this
+        status.houseCount = 0
+        status.mortgaged = true
     }
 
-    // 抵当解除
     settlementProperty(property: Property): void {
-        if (!this.propertyStatuses.has(property.name)) {
-            throw new Error()
-        }
-        this.propertyStatuses.get(property.name)!.mortgaged = false
+        const status: PropertyStatus = this.board.getPropertyStatus(property)
+        status.mortgaged = false
     }
 
-    isMortgaged(property: Property): boolean {
-        if (!this.propertyStatuses.has(property.name)) {
-            throw new Error()
-        }
-        return this.propertyStatuses.get(property.name)!.mortgaged
+    calculateTotalValuation(): number {
+        return Board.properties
+            .map(property => this.board.getPropertyStatus(property))
+            .filter(status => status.owner !== undefined && status.owner.id === this.id)
+            .map(status => status.calculateValuation())
+            .reduce((previousValue, currentValue) => previousValue + currentValue, 0)
     }
 
-    // 家を建てる
-    buildHouses(property: Property, count?: number): void {
-        this.addHouseCount(property, count ?? 1)
-    }
-
-    // 家を売る
-    sellHouses(property: Property, count: number): void {
-        this.addHouseCount(property, -(count ?? 1))
-    }
-
-    // 家を追加する
-    private addHouseCount(property: Property, count: number) {
-        if (!property.canBuildHouse()) {
-            throw new Error()
-        }
-        if (!this.propertyStatuses.has(property.name)) {
-            throw new Error()
-        }
-        const status: PropertyStatus = this.propertyStatuses.get(property.name)!
-
-        const afterHouseCount: number = status.houseCount + count
-        if (afterHouseCount < 0 || afterHouseCount > 5) {
-            throw new Error()
-        }
-        status.houseCount = afterHouseCount
-    }
-
-    // 家の数
-    getHouseCount(property: Property): number {
-        if (!this.propertyStatuses.has(property.name)) {
-            throw new Error()
-        }
-        return this.propertyStatuses.get(property.name)!.houseCount
-    }
-
-    // 評価額を算出する
-    calculatePresentValue(property: Property): number {
-        if (!this.propertyStatuses.has(property.name)) {
-            throw new Error()
-        }
-        const status: PropertyStatus = this.propertyStatuses.get(property.name)!
-
-        return status.calculatePresentValue()
-    }
-
-    calculateAllPresentValue(): number {
-        return Array.from(this.propertyStatuses.values()).reduce((previousValue, currentValue) => previousValue + currentValue.calculatePresentValue(), 0)
-    }
-
-    getTotalAssets(): number {
-        return this.money + this.calculateAllPresentValue()
+    isOwnedProperty(property: Property): boolean {
+        return this.board.getPropertyStatus(property).owner === this
     }
 }
